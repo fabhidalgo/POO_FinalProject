4f6cc78e836b482fb2415f8ff90902b7f844d882
Fabrizio Hidalgo <fabrizio13.fhn@gmail.com>
2025-05-21 00:27:18 -0500

    Database conexion

---
+++ src/BusinnesEntity/UserBE.java
@@ -0,0 +1,45 @@
+package BusinnesEntity;
+public class UserBE {
+    private String ID;
+    private String Name;
+    private String ApellidoP;
+    private String ApellidoM;
+
+    public UserBE() {
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getApellidoP() {
+        return ApellidoP;
+    }
+
+    public void setApellidoP(String ApellidoP) {
+        this.ApellidoP = ApellidoP;
+    }
+
+    public String getApellidoM() {
+        return ApellidoM;
+    }
+
+    public void setApellidoM(String ApellidoM) {
+        this.ApellidoM = ApellidoM;
+    }
+    
+    
+            
+}
